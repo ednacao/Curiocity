bfd2b68be7572e3d2c8a86abfda5bad6700f7ac2
Edna Cao <edna@salame.local>
2015-03-08 16:07:56 -0700

    object literal model

---
+++ static/js/secretSpaces.js
@@ -0,0 +1,20 @@
+var secretSpaces = {
+	init: function(){
+		this.events();
+	},
+	events: function(){
+		var self = this;
+		$('.get-location').on("click", function(e){
+			e.preventDefault();
+			self.getLocation();
+		}
+	},
+	getLocation: function(){
+		if (navigator.geolocation) {
+    		navigator.geolocation.getCurrentPosition(showPosition);
+		}	else { 
+	    	console.log ("Geolocation is not supported by this browser.");
+		}
+	}
+}
+
