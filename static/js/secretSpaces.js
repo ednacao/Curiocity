38fda3ee3837544d130093008439c12ba89c99e4
Edna Cao <edna@salame.local>
2015-03-09 13:17:50 -0700

    refactoring js

--- static/js/secretSpaces.js
+++ static/js/secretSpaces.js
@@ -6,42 +6,50 @@ var secretSpaces = {
 	},
 
 	events: function(){
+		debugger;
 		var self = this;
-		$('.get-location').on("click", function(e){
+		$('#get-location').on("click", function(e){
 			e.preventDefault();
 			self.getLocation();
 		});
 	},
 
+	showPosition: function(longitude, latitude){
+		$('.container').append("<input id='bomb' type='hidden' name='bomb' value=" + longitude + "," + latitude + "/>");
+	},
+
+	getCoords: function(position){
+		$.get("/pushcoords?lat=" + position.coords.latitude + 
+				"&long=" + position.coords.longitude, function(data){
+		    	// came back from server
+		    	console.log("I came back from the server!");
+		    	console.log(data);
+		    	debugger;
+		});
+	},
+
+	storePosition: function(position){
+		var self = this;
+		alert("STORE POSITION");
+		debugger;
+		var coordinates = "Latitude: " + position.coords.latitude + 
+		    "Longitude: " + position.coords.longitude;
+		    var longitude = position.coords.longitude;
+		    var latitude = position.coords.latitude;
+		self.showPosition(longitude, latitude); //FIXME - undefined is not a function, 'this' is a window (should be an object)
+		self.getCoords(position); //FIXME - undefined is not a function
+	},
+
 	getLocation: function(){
+		debugger;
 		if (navigator.geolocation) {
-    		navigator.geolocation.getCurrentPosition(showPosition);
+    		navigator.geolocation.getCurrentPosition(this.storePosition);
 		}	else { 
 	    	console.log ("Geolocation is not supported by this browser.");
 		}
 	},
 
-	// showPosition: function(){
-	// 	x.innerHTML = "Latitude: " + position.coords.latitude + 
-	// 	    "Longitude: " + position.coords.longitude;
-	// 	    var longitude = position.coords.longitude;
-	// 	    var latitude = position.coords.latitude;
-	// 	}
-	// },
 
-	// storePosition: function(){
-	// 	$('.container').append("<input id='bomb' type='hidden' name='bomb' value=" + longitude + "," + latitude + "/>");
-	// 	};
-	// },
-
-	// getCoords: function(){
-	// 	$.get("/pushcoords?lat=" + position.coords.latitude + 
-	// 			"&long=" + position.coords.longitude, function(data){
-	// 	    	// came back from server
-	// 	    	console.log("I came back from the server!");
-	// 	    	console.log(data);
-	// 	    	debugger;
-	// });
 };
 
 $(document).ready(function() {
