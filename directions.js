29a609495002409d313cd0eb15ff9de72b2920a4
Edna Cao <edna@salame.local>
2015-03-08 22:29:04 -0700

    added getCoords function

---
+++ directions.js
@@ -0,0 +1,5 @@
+console.log(latitude, longitude);
+
+// THIS IS THE PUBLIC ACCESS TOKEN!!!
+L.mapbox.accessToken = "pk.eyJ1IjoiZWNhbyIsImEiOiJHVEFEM1NnIn0.6hb7Mp5jlFNiu22rsZkDUg";
+L.mapbox.map('map-one', 'ecao.lce9cd80');
--- static/js/secretSpaces.js
+++ static/js/secretSpaces.js
@@ -2,19 +2,34 @@ var secretSpaces = {
 	init: function(){
 		this.events();
 	},
+
 	events: function(){
 		var self = this;
 		$('.get-location').on("click", function(e){
 			e.preventDefault();
 			self.getLocation();
-		}
+		});
 	},
+
 	getLocation: function(){
 		if (navigator.geolocation) {
     		navigator.geolocation.getCurrentPosition(showPosition);
 		}	else { 
 	    	console.log ("Geolocation is not supported by this browser.");
 		}
-	}
-}
+	},
 
+	getCoords: function(){
+		$.get("/pushcoords?lat=" + position.coords.latitude + 
+				"&long=" + position.coords.longitude, function(data){
+		    	// came back from server
+		    	console.log("I came back from the server!");
+		    	console.log(data);
+		    	debugger;
+	})
+};
+
+
+$(document).ready(function() {
+  secretSpaces.init();
+});
