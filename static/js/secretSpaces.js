3ff15501a73c9af8ea870510336ebc99125df2d0
Edna Cao <edna@salame.local>
2015-03-09 00:53:17 -0700

    debugging functions with object literal model

--- static/js/secretSpaces.js
+++ static/js/secretSpaces.js
@@ -1,3 +1,5 @@
+// this js file is referenced in base.html
+
 var secretSpaces = {
 	init: function(){
 		this.events();
@@ -19,16 +21,28 @@ var secretSpaces = {
 		}
 	},
 
-	getCoords: function(){
-		$.get("/pushcoords?lat=" + position.coords.latitude + 
-				"&long=" + position.coords.longitude, function(data){
-		    	// came back from server
-		    	console.log("I came back from the server!");
-		    	console.log(data);
-		    	debugger;
-	})
-};
+	// showPosition: function(){
+	// 	x.innerHTML = "Latitude: " + position.coords.latitude + 
+	// 	    "Longitude: " + position.coords.longitude;
+	// 	    var longitude = position.coords.longitude;
+	// 	    var latitude = position.coords.latitude;
+	// 	}
+	// },
 
+	// storePosition: function(){
+	// 	$('.container').append("<input id='bomb' type='hidden' name='bomb' value=" + longitude + "," + latitude + "/>");
+	// 	};
+	// },
+
+	// getCoords: function(){
+	// 	$.get("/pushcoords?lat=" + position.coords.latitude + 
+	// 			"&long=" + position.coords.longitude, function(data){
+	// 	    	// came back from server
+	// 	    	console.log("I came back from the server!");
+	// 	    	console.log(data);
+	// 	    	debugger;
+	// });
+};
 
 $(document).ready(function() {
   secretSpaces.init();
