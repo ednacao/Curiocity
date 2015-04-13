8fecd2a141a8c911531b51f75b4c75a6614ada44
Edna Cao <edna@salame.local>
2015-04-13 16:22:08 -0700

    organizing screenshots

---
+++ static/js/directions.js
@@ -0,0 +1,11 @@
+
+// THIS IS THE PUBLIC ACCESS TOKEN!!!
+L.mapbox.accessToken = "pk.eyJ1IjoiZWNhbyIsImEiOiJHVEFEM1NnIn0.6hb7Mp5jlFNiu22rsZkDUg";
+L.mapbox.map('map-one', 'ecao.lce9cd80');
+
+// function getDirections(){
+// 	alert("I'm inside getDirections()!");
+// 	$.get("http://api.tiles.mapbox.com/v4/directions/ecao.lce9cd80/{longitude},{latitude};{profile_longitude},{profile_latitude}.json?access_token=pk.eyJ1IjoiZWNhbyIsImEiOiJHVEFEM1NnIn0.6hb7Mp5jlFNiu22rsZkDUg", function(data){
+// 		alert('tings');
+// 	});
+// }
---
+++ static/js/profileMap.js
@@ -0,0 +1,2 @@
+"""This file will eventually contain the js sript for the mapbox
+code from profile.html"""
